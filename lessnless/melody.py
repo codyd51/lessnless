a3dc972e48ccad96213e5fdae28ba177fd320f72
Phillip Tennen <pt@datatheorem.com>
2017-10-22 23:55:54 -0400

    implement progression strategies

--- lessnless/melody.py
+++ lessnless/melody.py
@@ -3,25 +3,48 @@ from mingus.containers.note import Note
 import mingus.core.scales as scales
 
 
-class MelodyNote(object):
-    def __init__(self, note, beat_count):
-        self.note = note
+class Progression(object):
+    def __init__(self, beat_count, chord):
         self.beat_count = beat_count
+        self.chord = chord
+        self._generated_note_count = 0
 
-    def __repr__(self):
-        return '<{}, {} beats>'.format(self.note, self.beat_count)
+    def generate_notes(self):
+        raise NotImplementedError('Use concrete Progression subclass')
+
+    def _get_starting_note(self):
+        copy_note = random.choice(self.chord.notes)
+        octave_jump = 1
+        if random.choice([True, False]):
+            octave_jump = 2
+        starting_note = Note(name=copy_note.name, octave=copy_note.octave + octave_jump)
+        return starting_note
+
+    def _get_next_note_length(self):
+        # first, check if we're done generating notes
+        if self._generated_note_count == self.beat_count:
+            return 0
+
+        note_lengths = range(1, 4)
+        current_note_length = random.choice(note_lengths)
+        # was this more than beats we have left?
+        overlap = self._generated_note_count + current_note_length - self.beat_count
+        if overlap > 0:
+            # bind note length to beats we have left
+            current_note_length -= overlap
+        self._generated_note_count += current_note_length
+        return current_note_length
 
 
-class Melody(object):
-    def __init__(self, chord):
-        self.notes = self.get_notes(16, chord)
+from song import Song
 
-    def get_notes(self, beat_count, chord):
+
+class StepProgression(Progression):
+    def generate_notes(self):
         notes = []
-        copy_note = random.choice(chord.notes)
-        starting_note = Note(name=copy_note.name, octave=copy_note.octave + 2)
+        starting_note = self._get_starting_note()
 
-        scale_notes = scales.Major('C').ascending()
+        scale_notes = Song.get_c_scale()
         current_scale_idx = scale_notes.index(starting_note.name)
         current_raw_note = starting_note
 
